2b41e449935a82045e2734118381cdff3e8838b1
Dylan Blake <drblake@cs.ualberta.ca>
2015-04-02 19:47:30 -0600

    WE DID A LOT OKAY CONNER????????? (all 6 functions work with btree and hash. need to do shell script and time documenting

--- main.py
+++ main.py
@@ -8,34 +8,149 @@ import os
 # This will display the menu and handle input to the menu
 # after creating/population tables.
 def main():
-#    print("Please login before proceeding.")
-#    sql = None
-#    while(True):
-#        try:
-#            user = input("User [%s]:" % getpass.getuser())
-#            if not user:
-#                user = getpass.getuser()
-#            passw = getpass.getpass("Pass:")
-#
-#            # create a new instance of a connection object
-#            sql = sqlFile.SqlConnection(user, passw)
-#
-#            break
-#        except:
-#            print("Oops, try again!")
-#            continue
-#
-    # Waring if no arguments are used
     if(len(sys.argv) != 2):
         print("Warning: Arguments not properly specified!")
         print("First argument creates specifies database type (btree/hash/indexfile.")
         print("Quitting")
         sys.exit(10)
+
+    db = open_db()
+    try:
+        answer = open("answers", 'w')
+    except Exception as Error:
+        print(Error)
+
+    while(True):
+        print (
+"""----------------------------------------
+Please Select from the following:
+        1:Create and Populate Database
+        2:Retrieve records with Key
+        3:Retrive records with Data
+        4:Retrieve records with Key Range
+        5:Destroy Database
+        6:Quit""")
+        
+        choice = sqlFile.getNumber("Choice (1-6): ",1,1, 6, 1)
+
+        if choice == 1:
+            print("Creating/Populating Database")
+            create(10000,10000000, db)
+        elif choice == 2:
+            print("Retrieving Records with Key:")
+            key(db, answer)
+        elif choice == 3:
+            print("Retrieving Records with Data:")
+            data(db, answer)
+        elif choice == 4:
+            print("Retrieving Records with Key Range:")
+            keyRange(db, answer)
+        elif choice == 5:
+            print("Destroying Database")
+            db = destroy(db)
+        elif choice == 6:
+            print("Good Bye.")
+            break
+        else:
+            print ("Invalid Input!")
+    db.close()
+    os.remove("tmp/sbaergen_db")
+    answer.close()
     
+def create(length, seed, db):
+    random.seed(seed)
+
+    for value in range(length):
+        key_range = integer_generator()
+        value_range = integer_generator()
+        new_key = ""
+        new_value = ""
+        for increment in range(key_range):
+            new_key += str(char_generator())
+        for increment in range(value_range):
+            new_value += str(char_generator())
+        try:
+            Data = db[new_key]
+        except:
+            new_key = new_key.encode(encoding ='UTF-8')
+            new_value = new_value.encode(encoding = 'UTF-8')
+            db[new_key] = new_value
+       
+    print("Database Populated Successfully")
+
+
+def key(db, answer):
+    search_key = input("Enter the key value you wish to search for: ")               
+    search_key = search_key.encode(encoding ='UTF-8')
+    try:
+        data = db[search_key]
+        answer.write(search_key.decode(encoding ='UTF-8') + '\n')
+        answer.write(data.decode(encoding = 'UTF-8') + '\n')
+        answer.write('\n')
+        return
+    except Exception as error:
+        print(error)
+        print("Key does not exist")
+    return
+
+def data(db, answer):
+    search_data = input("Enter the data you wish to search for: ")
+    search_data = search_data.encode(encoding = 'UTF-8')
+    try:
+        for key in db:
+            if db[key] == search_data:
+                answer.write(key.decode(encoding ='UTF-8') + '\n')
+                answer.write(search_data.decode(encoding = 'UTF-8') + '\n')
+                answer.write('\n')
+    except Exception as error:
+        print(error)
+        print("Data does not exist")
+
+    return
+
+# Get Range of data
+def keyRange(db, answer):
+    search_key_min = input("Enter the minimum key value you wish to search for: ")       
+    search_key_max = input("Enter the maximum key value you wish to search for: ")        
+    search_key_min = search_key_min.encode(encoding ='UTF-8')
+    search_key_max = search_key_max.encode(encoding ='UTF-8')
+    count = 0
+    try:
+        for key in db:
+            if key > search_key_min and key < search_key_max:
+                count+=1
+                solution_key = key
+                solution_data = db[solution_key]
+                answer.write(solution_key.decode(encoding ='UTF-8') + '\n')
+                answer.write(solution_data.decode(encoding = 'UTF-8') + '\n')
+                answer.write('\n')
+                print(count)
+        print("Result Recorded")
+        return 
+    except Exception as error:
+        print(error)
+        print("Key does not exist")
+    
+    return
+
+# Destroy database, Clear Answer
+def destroy(db):
+    db.close()
+    db = open_db()
+    return db
+
+def getTime():
+    return
+    
+def integer_generator():
+    return random.randint(64,127)
+def char_generator():
+    return chr(random.randint(97,122))
+
+def open_db():
     DATABASE = "tmp/sbaergen_db"
     if not os.path.exists("tmp/"):
         os.makedirs("tmp/") 
-    print("HELLO")
     if(str(sys.argv[1]).lower() == "btree"):     
         try:
             db = bsddb.btopen(DATABASE, 'w')
@@ -53,103 +168,8 @@ def main():
         print("Invalid Argument. Please try again!")
         print(str(sys.argv[1]).lower())
         sys.exit(10)
-    
-    # Drop & Create Tables
-#    if(len(sys.argv) >= 2):
-#        print("Dropping / Creating Tables")
-#        print("Using the argument " + str(sys.argv[1]))
-#        sql.executeFromFile(str(sys.argv[1]))
-
-    # Populate tables
-#    if(len(sys.argv) >= 3):
-#        print("Populate Tables")
-#        print("Using the argument " + str(sys.argv[2]))
-#        sql.executeFromFile(str(sys.argv[2]))
 
     print("")  # put some space between info above and start of menu
-    while(True):
-        print (
-"""----------------------------------------
-Please Select from the following:
-        1:Create and Populate Database
-        2:Retrieve records with Key
-        3:Retrive records with Data
-        4:Retrieve records with Key Range
-        5:Destroy Database
-        6:Quit""")
-
-        choice = sqlFile.getNumber("Choice (1-6): ",1,1, 6, 1)
-
-        if choice == 1:
-            print("Creating/Populating Database")
-            create(10000,10000000, db)
-        elif choice == 2:
-            print("Retrieving Records with Key:")
-            key()
-        elif choice == 3:
-            print("Retrieving Records with Data:")
-            data()
-        elif choice == 4:
-            print("Retrieving Records with Key Range:")
-            keyRange()
-        elif choice == 5:
-            print("Destroying Database")
-            destroy()
-        elif choice == 6:
-            destroy()
-            print("Good Bye.")
-            break
-        else:
-            print ("Invalid Input!")
-    finish()
-
-def create(length, seed, db):
-    random.seed(seed)
-
-    for value in range(length):
-        key_range = integer_generator()
-        new_key = ""
-        new_value = ""
-        for increment in range(key_range):
-            new_key += str(char_generator())
-        for increment in range(key_range):
-            new_value += str(char_generator())
-
-        new_key = new_key.encode(encoding ='UTF-8')
-        new_value = new_value.encode(encoding = 'UTF-8')
-    
-        db[new_key] = new_value
-    print("KEY: ", new_key, "\n")
-    print("VALUE: ", new_value, "\n")
-    print("\n")
-    try:
-        db.close()
-        print("Database Populated Successfully")
-    except Exception as Error:
-        print(Error)
-
-def key():
-    return
-def data():
-    return
-# Get Range of data
-def keyRange():
-    return
-
-# Destroy database, Clear Answer
-def destroy():
-    return
-
-def getTime():
-    return
-    
-def integer_generator():
-    return random.randint(64,127)
-def char_generator():
-    return chr(random.randint(97,122))
-
-def finish():
-    os.remove("tmp/sbaergen_db")
-    return
-
+    return db
+ 
 main()  # run the main function
